b46be100f18c4049d95825c4556107dcf56cd5b1
Logan Simila <Lsimila@ivytech.edu>
2026-04-07 04:31:04 -0400

    Finalizing assignment 4.1

---
+++ Module 2 Programming Assignment/Assignment 4.1.py
@@ -0,0 +1,21 @@
+# importing tool/lib 's
+from random import randint
+
+# Input
+guess = str(input("Guess the secret number (1 - 10): "))
+secret = randint(1, 10)
+
+# Logic (runs once, didn't say use a loop :/ )
+if guess < secret:
+    print("too low")
+elif guess > secret:
+    print("too high")
+else:
+    print("just right")    
+
+'''
+Required: randnum = "secret"
+Required: use if/else/elif
+Required: use "too low" and "too high" and "just right"
+Required: use var "guess"
+'''
