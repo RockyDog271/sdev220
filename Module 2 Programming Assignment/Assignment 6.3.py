3de4cee1a61a017612906129d27f75932db912a5
Logan Simila <Lsimila@ivytech.edu>
2026-04-07 05:02:03 -0400

    Last submission (6.3)

---
+++ Module 2 Programming Assignment/Assignment 6.3.py
@@ -0,0 +1,19 @@
+guess_me = 5
+for number in range(10):
+    if number < guess_me:
+        print("too low")
+    elif number == guess_me:
+        print("found it!")
+        break
+    else:
+        print("oops")
+        break
+
+
+'''
+Assign the value 5 to the variable guess_me.
+Use a for loop to iterate a variable called number over range(10).
+If number is less than guess_me, print 'too low'.
+If it equals guess_me, print found it! and then break out of the for loop.
+If number is greater than guess_me, print 'oops' and then exit the loop.
+'''
